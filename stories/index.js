dd5c284080768aa63047fed2ba414cb28f507a3f
uixcrazy <uixcrazy@gmail.com>
2018-10-04 16:07:30 +0700

    chore (storybook): Add Custom Head Tags

--- stories/index.js
+++ stories/index.js
@@ -1,3 +1,72 @@
-// import '../assets/normalize.css';
-import './stories.style.css';
-import './react-jss.stories';
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import WithNotes from '../.storybook/addons/notes/WithNotes';
+import WithStorySource from '../.storybook/addons/storysource/WithStorySource';
+/* eslint-disable */
+import GettingStarted from '../src/getting-started';
+import GettingStartedReadme from '../src/getting-started/README.md';
+import GettingStartedContent from '!!raw-loader!../src/getting-started';
+
+import ExpandedProperties from '../src/expanded-properties';
+import ExpandedPropertiesReadme from '../src/expanded-properties/README.md';
+import ExpandedPropertiesContent from '!!raw-loader!../src/expanded-properties';
+
+import Theming from '../src/theming';
+import ThemingReadme from '../src/theming/README.md';
+import ThemingContent from '!!raw-loader!../src/theming';
+
+import SSRendering from '../src/server-side-rendering';
+import SSRenderingReadme from '../src/server-side-rendering/README.md';
+import SSRenderingContent from '!!raw-loader!../src/server-side-rendering';
+
+import Decorators from '../src/decorators';
+import DecoratorsReadme from '../src/decorators/README.md';
+import DecoratorsContent from '!!raw-loader!../src/decorators';
+
+import JssCustom from '../src/jss-custom';
+import JssCustomReadme from '../src/jss-custom/README.md';
+import JssCustomContent from '!!raw-loader!../src/jss-custom';
+
+storiesOf('React JSS', module)
+.add('1. Getting Started', () =>(
+  <WithNotes notes={GettingStartedReadme}>
+    <WithStorySource storysource={GettingStartedContent}>
+      <GettingStarted />
+    </WithStorySource>
+  </WithNotes>
+))
+.add('2. Expanded Properties', () =>(
+  <WithNotes notes={ExpandedPropertiesReadme}>
+    <WithStorySource storysource={ExpandedPropertiesContent}>
+      <ExpandedProperties />
+    </WithStorySource>
+  </WithNotes>
+))
+.add('3. Theming', () =>(
+  <WithNotes notes={ThemingReadme}>
+    <WithStorySource storysource={ThemingContent}>
+      <Theming />
+    </WithStorySource>
+  </WithNotes>
+))
+.add('4. Server-side rendering', () =>(
+  <WithNotes notes={SSRenderingReadme}>
+    <WithStorySource storysource={SSRenderingContent}>
+      <SSRendering />
+    </WithStorySource>
+  </WithNotes>
+))
+.add('5. Custom setup', () =>(
+  <WithNotes notes={JssCustomReadme}>
+    <WithStorySource storysource={JssCustomContent}>
+      <JssCustom />
+    </WithStorySource>
+  </WithNotes>
+))
+.add('5... Decorators', () =>(
+  <WithNotes notes={DecoratorsReadme}>
+    <WithStorySource storysource={DecoratorsContent}>
+      <Decorators />
+    </WithStorySource>
+  </WithNotes>
+))
